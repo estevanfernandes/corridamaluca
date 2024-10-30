6ee8b297e4b38b7616532af9c16dab4307ea6354
Estevan Fernandes <estevan.stolfo05@gmail.com>
2024-10-29 21:04:07 -0300

    Versao base do codigo

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print("Hello, World!")
-
-print(5+5)
+import pygame
+pygame.init()  
+tamanho = (1000, 592)
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho )
+pygame.display.set_caption("Corrida Maluca")
+branco = (255,255,255)
+preto = (0,0,0)
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            quit()
+            
+    tela.fill( branco )
+    pygame.display.update()
+    clock.tick(60)
+    
+pygame.quit()
